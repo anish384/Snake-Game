9e7b002d53a755531cd68513e58147f9a268c3fa
anish384 <desaianish351@outlook.com>
2024-09-04 16:06:10 +0000

    04-09-2024 comment added by anish desai at 21:36

--- Turtle_SnakeGame1.py
+++ Turtle_SnakeGame1.py
@@ -6,6 +6,7 @@ from Turtle_SnakeGame3 import Food
 from Turtle_SnakeGame4 import S_Board
 
 # this is called comment
+# this is another comment
 
 screen = Screen()
 screen.setup(width=600, height=600)
